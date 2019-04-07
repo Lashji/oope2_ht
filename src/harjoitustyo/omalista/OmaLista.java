e01dc108e4e0cae23b30b7ad803da309ace9c9b2
lashj <lassi.palojarvi@outlook.com>
2019-04-07 12:03:09 +0300

    omalista

--- src/harjoitustyo/omalista/OmaLista.java
+++ src/harjoitustyo/omalista/OmaLista.java
@@ -1,10 +1,7 @@
 package harjoitustyo.omalista;
 
 import harjoitustyo.apulaiset.Ooperoiva;
-
-import java.util.Collection;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.LinkedList;
 import java.util.ListIterator;
 
@@ -17,59 +14,39 @@ public class OmaLista<E> extends LinkedList<E> implements Ooperoiva<E> {
 
     @Override
     public boolean lisaa(E uusi) {
-<<<<<<< HEAD
-        return add(uusi);
-=======
 
 //        Instance of palauttaa false jos arvo null. Joten null arvon tarkistaminen ei ole erikseen tarpeen.
         if (uusi instanceof Comparable) {
 
-            add(uusi);
+            if (add(uusi)) {
 
-            Collections.sort(this, new Comparator<E>() {
-                @Override
-                public int compare(E e, E t1) {
+                Collections.sort(this, (e, t1) -> {
+                    if (e instanceof Comparable && t1 instanceof Comparable) {
 
-                    if (e instanceof String && t1 instanceof String) {
-
-                        String t = (String) e;
-                        String t2 = (String) t1;
-
+                        Comparable t = (Comparable) e;
+                        Comparable t2 = (Comparable) t1;
 
                         return t.compareTo(t2);
-
-
                     }
                     return 0;
-                }§
-            });
-            return true;
+                });
+                return true;
+            }
         }
-
         return false;
->>>>>>> 4e5e515f6afbc372774f3d40b848e9066b7a562b
     }
 
     @Override
     public int poista(E poistettava) {
-<<<<<<< HEAD
-
-        ListIterator iterator = new ListIterator();
-
-
-
-
-=======
+        ListIterator<E> itr = this.listIterator();
         int maara = 0;
 
-        for (E e : this) {
-            if (e.equals(poistettava)) {
-                remove(e);
+        while (itr.hasNext()) {
+            if (itr.next().equals(poistettava)) {
+                itr.remove();
                 maara++;
             }
         }
-
         return maara;
->>>>>>> 4e5e515f6afbc372774f3d40b848e9066b7a562b
     }
 }
